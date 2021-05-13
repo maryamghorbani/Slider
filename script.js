7340c004fbb8cf0a3c501b5c45f12c5c43aac2f8
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-13 23:42:58 +0430

    add second slider

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ new slider({
     },
     auto : 3000
 })
+
+
+new slider({
+    el : document.querySelector('#sliders2'),
+    slideClass : 'slider',
+    currentSlider : (slider) => {
+        console.log(slider);
+    },
+    auto : 1000
+})
