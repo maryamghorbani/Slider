bbc448c470c786f3afeff46ecc5a9e1ffae4039b
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-13 22:29:21 +0430

    insert dots after slider

--- slider.js
+++ slider.js
@@ -18,7 +18,6 @@ class slider {
 
         Number.isInteger(auto) ? this.auto = auto : this.auto = 0;
         this.sliders = [ ...sliderElement.children].filter(elm => elm.classList.contains(slideClass));
-        console.log(sliders);
     }
 
     createNextAndPrevBtns () {
@@ -36,12 +35,22 @@ class slider {
     decrementSlide = () => this.showSlides(this.sliderIndex -= 1)
 
     createDots() {
+        let { el : sliderElement } = this.options;
 
+        let dotElements = [ ... this.sliders ].map((slider , index) => `<span class="dot" deta-slide="${index+1}"></span>`)
+
+        let dots = document.createElement('div');
+        dots.classList.add('dots');
+        dots.innerHTML = `${dotElements.join('')}`// تبدیل آیتم های یک رشته به یک لیست
+
+        sliderElement.after(dots)
+
+        console.log(dots)
     }
 
     showSlides(number) {
         if (number > this.sliders.length) this.sliderIndex = 1;
         if (number < 1) this.sliderIndex = this.sliders.length;
-        
+
     }
 }
